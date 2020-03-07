44c053ebe44a43db2e0a9aa62bca864d40eb1979
erickjcalder <47425842+erickjcalder@users.noreply.github.com>
2020-03-06 17:25:58 -0700

    Iteration 1 commit
    Added basic login functionality, tweaked style sheets, reorganized file tree, fixed bugs

---
+++ scripts/login.js
@@ -0,0 +1,57 @@
+var users = ["Admin", "TestUser"];
+var passes = ["AdminPass", "TestPass"];
+
+function testAlert() {
+  alert("Code reached");
+}
+
+function validate()
+    {
+      var username = document.getElementById("name");
+      var password = document.getElementById("psw");
+
+      username = username.value;
+      password = password.value;
+
+      if(users.indexOf(username) > -1) {
+        var userIndex = users.indexOf(username);
+      }
+
+      if(username == "" ||password == "")
+      {
+        alert("Username and Password required");
+        return false;
+      }
+
+      else if(users.indexOf(username) > -1 && passes[userIndex] == password)
+      {
+        alert("Login success!");
+        return true;
+      }
+      else{
+        alert("Incorrect Username or Password");
+        return false;
+      }
+    }
+
+    function myFunction(){
+      let x = document.getElementById("psw");
+      if (x.type === "password") {
+        x.type = 'text';
+      } else {
+        x.type = "password";
+      }
+    }
+
+    function loginBoxdisplay(v){
+      
+      if (v == 1){
+        document.getElementById('id01').style.display='none';
+        document.getElementById('loginbox').style.visibility = 'visible';
+      }
+      else if(v == 0){
+        document.getElementById('id01').style.display='block';
+        document.getElementById('loginbox').style.visibility = 'hidden';
+      }
+    }
+    
