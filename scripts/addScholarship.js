3d6f655372e264e3d5dd1d147c2ddcde3c2ca4df
Kate Rivera <kateashley.rivera@ucalgary.ca>
2020-03-22 17:56:30 -0600

    finished array for course acronyms

--- scripts/addScholarship.js
+++ scripts/addScholarship.js
@@ -6,7 +6,20 @@ function deptArray() {
                 "ARST", "ARCH", "APLA", "ART", "ARHI", "ASHA", "ARTS", "ASTR", "ASPH",
                 "BCEM", "BIOL", "BMEN", "BIST", "BOTA", "VSEN", "BTMA", "CNST", "CMMB",
                 "CEST", "ENCH", "CHEM", "CHIN", "ENCI", "CMCL", "COMS", "MDCH", "CORE",
-                "CMDA", "ENCM", "CPSC", "COOP", "DNCE", "DCED", "DATA", "DEST", "DRAM",];
+                "CMDA", "ENCM", "CPSC", "COOP", "DNCE", "DCED", "DATA", "DEST", "DRAM",
+                "EASC", "EALS", "EAST", "ECOL", "ECON", "EDBT", "EDUC", "EDPS", "EDER",
+                "ENEL", "ENEE", "EESS", "ENER", "ENMG", "ENGG", "ENGL", "ENTI", "EVDA",
+                "EVDS", "EVDL", "EVDP", "ENEN", "ENSC", "FILM", "FNCE", "FINA", "FREN",
+                "GEOG", "GLGY", "ENGO", "GOPH", "GERM", "GRST", "GREK", "HSOC", "HTST",
+                "INDL", "INDG", "ISEC", "INNO", "INTR", "INTE", "IPHE", "ITAL", "JPNS",
+                "KNES", "LAND", "LANG", "LLAC", "LAST, LATI", "LWSI", "LAW", "LING",
+                "MGST", "ENMF", "MRSC", "MKTG", "MATH", "ENME", "MDGE", "MDPH", "MDSC",
+                "MDCN", "MHST", "MUED", "MUSI", "MUPF", "NANS", "NEUR", "NURS", "OPMA",
+                "OBHR", "ENPE", "PHIL", "PHED", "PHYS", "PLAN", "PLBI", "POLI", "PLMA",
+                "PSYC", "PPOL", "REAL", "RELS", "RMIN", "ROST", "RUSS", "SCPA", "SCIE",
+                "SLAV", "SOWK", "SOCI", "ENSF", "SENG", "SAST", "SPPH", "SPAN", "STAT",
+                "STST", "SGMA", "SCMA", "SUST", "SEDV", "TAP", "TOUR", "TRAN", "UNEX",
+                "UNIV", "UBST", "VETM", "WELL", "WMST", "ZOOL"];
     
         for(var i = 0; i < arr.length; i++)
         {
