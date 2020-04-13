4cf078a275ce2291a4815ef72c3f3667ee1231b3
Kate Rivera <47331446+katearevir@users.noreply.github.com>
2020-04-13 01:40:24 -0600

    nominate students scholarship dropdown, no if statement yet

--- public/scripts/nominateStudents.js
+++ public/scripts/nominateStudents.js
@@ -9,12 +9,18 @@ function getScholarships() {
         for (var i = 0; i < data.length; i++) {
             console.log(data[i].name);
         }
-        showScholarships(data);
+        showNominations(data);
     });
 }
 
-function showScholarshipNominations(){
-    var select = document.getElementById("nominateScholarship");
+function showNominations(scholarships){
+    var select = document.getElementById("scholarships");
 
-    
+    for (var i = 0; i < scholarships.length; i++) {
+        var option = document.createElement("OPTION")
+
+        var txt = document.createTextNode(scholarships[i].name);
+        option.appendChild(txt);
+        select.insertBefore(option, select.lastChild);
+    }
 }
